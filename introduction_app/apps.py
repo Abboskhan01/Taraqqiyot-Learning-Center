c72fd182ccdd093f8976692cab63648b0a40fc83
Abbos <abbosr180@gmail.com>
2022-09-29 14:07:37 +0500

    good

--- introduction_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class IntroductionAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'introduction_app'
--- register_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import regiter_view
-
-
-urlpatterns = [
-    path('', regiter_view, name='register_view'),
-
-]
